73db9afcd2451a7fee82dd223237c71cf38d8da4
Nitin Rayal <91653364+rayalnitin@users.noreply.github.com>
2025-09-04 19:41:49 +0530

    Second commit

---
+++ src/exceptions/root.ts
@@ -0,0 +1,1 @@
+// error message , status code  , error codes
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import express from "express"
+import express , {Express , Request , Response} from "express"
+import { PORT } from "./secrets"
+import rootRouter from "./routes/index"
+import { PrismaClient } from "@prisma/client"
 
-const app = express()
+const app:Express = express()
 
-app.get('/' , (req,res)=>{
-    res.send('Working')
+app.use(express.json())
+app.use('/api' , rootRouter)
+
+export const prismaClient = new PrismaClient({
+    log :['query']
 })
-app.listen(3000 , ()=>{
+
+app.listen(PORT , ()=>{
     console.log("App working")
 })
