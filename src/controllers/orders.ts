adb4c9dbe849285db03494cda64132be9f41f972
Nitin Rayal <91653364+rayalnitin@users.noreply.github.com>
2025-09-13 15:25:55 +0530

    Admin functionality added

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -149,3 +149,70 @@ export const getOrderById = async(req:Request , res:Response)=>{
         throw new notFoundException('Order not found' , ErrorCodes.ORDER_NOT_FOUND)
     }
 }
+
+export const listAllOrders = async(req:Request , res:Response)=>{
+    let whereClause = {}
+    const status = req.query.status
+    if(status){
+        whereClause={
+            status
+        }
+    }
+    const orders = await prismaClient.order.findMany({
+        where: whereClause,
+        skip: +(req.query.skip || 0),
+        take:5
+    })
+
+    res.json(orders)
+}
+export const changeStatus = async(req:Request , res:Response)=>{
+    try{
+        const existingOrder = await prismaClient.order.findFirstOrThrow({
+            where:{
+                id:+req.params.id
+            }
+        })
+
+        const order = await prismaClient.$transaction(async(tx)=>{
+            const order = await tx.order.update({
+                where:{
+                id: +req.params.id
+                },
+                data:{
+                    status:req.body.status
+                }
+            })
+            await tx.orderEvent.create({
+                data:{
+                    orderId: order.id,
+                    status: req.body.status
+                }
+            })
+        return order
+        })
+        res.json(order)
+    
+    }catch(error){
+        throw new notFoundException('Order not found' , ErrorCodes.ORDER_NOT_FOUND)
+    }
+}
+export const listUserOrders = async(req:Request , res:Response)=>{
+    let whereClause:any = {
+        userId: +req.params.id
+    }
+    const status = req.query.status
+    if(status){
+        whereClause={
+            ...whereClause,
+            status
+        }
+    }
+    const orders = await prismaClient.order.findMany({
+        where: whereClause,
+        skip: +(req.query.skip || 0),
+        take:5
+    })
+
+    res.json(orders)
+}
--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth";
 import { errorHandler } from "../error-handler";
-import { cancelOrder, createOrder, getOrderById, listOrder } from "../controllers/orders";
+import { cancelOrder, changeStatus, createOrder, getOrderById, listAllOrders, listOrder, listUserOrders } from "../controllers/orders";
+import { adminMiddleware } from "../middlewares/admin";
 
 export const orderRoutes:Router = Router();
 
 orderRoutes.post('/',[authMiddleware] , errorHandler(createOrder))
 orderRoutes.put('/:id/cancel',[authMiddleware] , errorHandler(cancelOrder))
-orderRoutes.get('/:id',[authMiddleware] , errorHandler(getOrderById))
+
 orderRoutes.get('/',[authMiddleware] , errorHandler(listOrder))
+
+orderRoutes.get('/index' , [authMiddleware , adminMiddleware] , errorHandler(listAllOrders))
+orderRoutes.get('/users/:id' , [authMiddleware , adminMiddleware] , errorHandler(listUserOrders))
+orderRoutes.put('/:id/status' , [authMiddleware , adminMiddleware] , errorHandler(changeStatus))
+
+orderRoutes.get('/:id',[authMiddleware] , errorHandler(getOrderById))
